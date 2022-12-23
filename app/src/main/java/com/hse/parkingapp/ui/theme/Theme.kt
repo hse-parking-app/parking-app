e2f6969fc65570c654e4dcad18466d561f97b496
Egor Kornev <boolgater@gmail.com>
2022-12-23 05:23:41 +0300

    Fully worked Compose theme added. Defined app colors (+ Dark theme) in Color.kt and app startup colors in themes.xml

--- app/src/main/java/com/hse/parkingapp/ui/theme/Theme.kt
+++ app/src/main/java/com/hse/parkingapp/ui/theme/Theme.kt
@@ -1,66 +1,96 @@
 package com.hse.parkingapp.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.DisposableEffect
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val LightColorScheme = lightColorScheme(
+    primary = light_primary,
+    onPrimary = light_onPrimary,
+    primaryContainer = light_primaryContainer,
+    onPrimaryContainer = light_onPrimaryContainer,
+    inversePrimary = light_inversePrimary,
+    secondary = light_secondary,
+    onSecondary = light_onSecondary,
+    secondaryContainer = light_secondaryContainer,
+    onSecondaryContainer = light_onSecondaryContainer,
+    tertiary = light_tertiary,
+    onTertiary = light_onTertiary,
+    tertiaryContainer = light_tertiaryContainer,
+    onTertiaryContainer = light_onTertiaryContainer,
+    background = light_background,
+    onBackground = light_onBackground,
+    surface = light_surface,
+    onSurface = light_onSurface,
+    surfaceVariant = light_surfaceVariant,
+    onSurfaceVariant = light_onSurfaceVariant,
+    surfaceTint = light_surfaceTint,
+    inverseSurface = light_inverseSurface,
+    inverseOnSurface = light_inverseOnSurface,
+    error = light_error,
+    onError = light_onError,
+    errorContainer = light_errorContainer,
+    onErrorContainer = light_onErrorContainer,
+    outline = light_outline,
+    outlineVariant = light_outline,
+    scrim = light_background
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = BackgroundBaseLight,
-    secondary = BackgroundNeutralLight,
-    tertiary = BackgroundAccentLight,
-
-    background = BackgroundBaseLight,
-    onPrimary = TextPrimaryLight,
-    onSecondary = TextAccentLight,
-    onTertiary = BackgroundBaseLight,
-
-    primaryContainer = BackgroundSlotColorLight,
-    secondaryContainer = BackgroundBrandLight
+private val DarkColorScheme = darkColorScheme(
+    primary = dark_primary,
+    onPrimary = dark_onPrimary,
+    primaryContainer = dark_primaryContainer,
+    onPrimaryContainer = dark_onPrimaryContainer,
+    inversePrimary = dark_inversePrimary,
+    secondary = dark_secondary,
+    onSecondary = dark_onSecondary,
+    secondaryContainer = dark_secondaryContainer,
+    onSecondaryContainer = dark_onSecondaryContainer,
+    tertiary = dark_tertiary,
+    onTertiary = dark_onTertiary,
+    tertiaryContainer = dark_tertiaryContainer,
+    onTertiaryContainer = dark_onTertiaryContainer,
+    background = dark_background,
+    onBackground = dark_onBackground,
+    surface = dark_surface,
+    onSurface = dark_onSurface,
+    surfaceVariant = dark_surfaceVariant,
+    onSurfaceVariant = dark_onSurfaceVariant,
+    surfaceTint = dark_surfaceTint,
+    inverseSurface = dark_inverseSurface,
+    inverseOnSurface = dark_inverseOnSurface,
+    error = dark_error,
+    onError = dark_onError,
+    errorContainer = dark_errorContainer,
+    onErrorContainer = dark_onErrorContainer,
+    outline = dark_outline,
+    outlineVariant = dark_outline,
+    scrim = dark_background
 )
 
 @Composable
 fun ParkingAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        // TODO: enable dark theme again :D !
-        darkTheme -> LightColorScheme
+        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
     val systemUiController = rememberSystemUiController()
-    SideEffect {
+    DisposableEffect(systemUiController, darkTheme) {
         systemUiController.setSystemBarsColor(
-            color = if (darkTheme) colorScheme.background else colorScheme.background,
-            /* TODO: Dark theme support */
-            darkIcons = !darkTheme,
+            color = colorScheme.background,
+            darkIcons = !darkTheme
         )
-    }
 
-//    val view = LocalView.current
-//    if (!view.isInEditMode) {
-//        SideEffect {
-//            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-//            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-//        }
-//    }
+        onDispose { }
+    }
 
     MaterialTheme(
         colorScheme = colorScheme,
