a06c6d2846bf293aa400d3596a12fd4c7421664c
pasha <pashamedvedev03@gmail.com>
2023-01-02 22:02:19 +0300

    create MainViewModel.kt and implement basic UI logic

---
+++ app/src/main/java/com/hse/parkingapp/ui/MainViewModel.kt
@@ -0,0 +1,56 @@
+package com.hse.parkingapp.ui
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class MainViewModel : ViewModel() {
+    val uiState = MutableStateFlow(AuthenticationState())
+
+    fun handleEvent(authenticationEvent: AuthenticationEvent) {
+        when (authenticationEvent) {
+            is AuthenticationEvent.ToggleAuthenticationMode -> {
+                toggleAuthenticationMode()
+            }
+            is AuthenticationEvent.UsernameChanged -> {
+                updateUsername(authenticationEvent.username)
+            }
+            is AuthenticationEvent.PasswordChanged -> {
+                updatePassword(authenticationEvent.password)
+            }
+        }
+    }
+
+    private fun toggleAuthenticationMode() {
+        val authenticationMode = uiState.value.authenticationMode
+        val newAuthenticationMode = if (authenticationMode == AuthenticationMode.SIGN_IN)
+            AuthenticationMode.SIGN_UP else AuthenticationMode.SIGN_IN
+        uiState.value = uiState.value.copy(
+            authenticationMode = newAuthenticationMode
+        )
+    }
+
+    private fun updateUsername(username: String) {
+        uiState.value = uiState.value.copy(
+            username = username
+        )
+    }
+
+    private fun updatePassword(password: String) {
+        val requirements = mutableListOf<PasswordRequirements>()
+
+        if (password.length > 7) {
+            requirements.add(PasswordRequirements.EIGHT_CHARACTERS)
+        }
+        if (password.any { it.isUpperCase() }) {
+            requirements.add(PasswordRequirements.CAPITAL_LETTER)
+        }
+        if (password.any { it.isDigit() }) {
+            requirements.add(PasswordRequirements.NUMBER)
+        }
+
+        uiState.value = uiState.value.copy(
+            password = password,
+            passwordRequirements = requirements.toList()
+        )
+    }
+}
