da6ff663abc1460b9ec20935f50377a214d329f1
pasha <pashamedvedev03@gmail.com>
2023-01-02 22:01:53 +0300

    create AuthenticationEvent.kt

---
+++ app/src/main/java/com/hse/parkingapp/ui/AuthenticationEvent.kt
@@ -0,0 +1,7 @@
+package com.hse.parkingapp.ui
+
+sealed class AuthenticationEvent {
+    object ToggleAuthenticationMode: AuthenticationEvent()
+    class UsernameChanged(val username: String): AuthenticationEvent()
+    class PasswordChanged(val password: String): AuthenticationEvent()
+}
