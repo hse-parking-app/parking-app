82cffe79c078f6a7e82c8abfedcaec70e13b84ea
pasha <pashamedvedev03@gmail.com>
2023-01-31 17:30:19 +0300

    populate SelectorState with more useful fields

--- app/src/main/java/com/hse/parkingapp/ui/main/SelectorState.kt
+++ app/src/main/java/com/hse/parkingapp/ui/main/SelectorState.kt
@@ -2,10 +2,18 @@ package com.hse.parkingapp.ui.main
 
 import java.time.Month
 import java.time.MonthDay
+import java.time.format.TextStyle
+import java.util.*
 
 data class SelectorState(
     val currentMonth: Month = MonthDay.now().month,
     val selectedDay: MonthDay = MonthDay.of(MonthDay.now().month, MonthDay.now().dayOfMonth),
     val selectedTime: Int = 0,
-    val selectedSlot: Slot? = null
+    val selectedSlot: Slot? = null,
+    val currentMonthName: String = currentMonth
+        .getDisplayName(TextStyle.FULL_STANDALONE, Locale.getDefault())
+        .replaceFirstChar { it.titlecase(Locale.getDefault()) },
+    val currentMonthLength: Int = currentMonth.length(false),
+    val previousMonthLength: Int = (currentMonth - 1).length(false),
+    val nextMonthLength: Int = (currentMonth + 1).length(false)
 )
