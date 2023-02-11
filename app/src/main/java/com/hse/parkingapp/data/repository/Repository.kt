3c9b1669f296e08925fdf37a3cd23a8be65dd896
pasha <pashamedvedev03@gmail.com>
2023-02-11 19:59:09 +0300

    add new suspend functions to get data from backend

--- app/src/main/java/com/hse/parkingapp/data/repository/Repository.kt
+++ app/src/main/java/com/hse/parkingapp/data/repository/Repository.kt
@@ -2,10 +2,28 @@ package com.hse.parkingapp.data.repository
 
 import com.hse.parkingapp.model.building.Building
 import com.hse.parkingapp.data.network.RetrofitInstance
+import com.hse.parkingapp.model.level.Level
+import com.hse.parkingapp.model.spot.Spot
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Response
 
 class Repository {
     suspend fun getBuildings(): Response<List<Building>> {
-        return RetrofitInstance.api.getBuildings()
+        return withContext(Dispatchers.IO) {
+            RetrofitInstance.api.getBuildings()
+        }
+    }
+
+    suspend fun getBuildingLevels(buildingId: String): Response<List<Level>> {
+        return withContext(Dispatchers.IO) {
+            RetrofitInstance.api.getBuildingLevels(buildingId)
+        }
+    }
+
+    suspend fun getLevelSpots(levelId: String): Response<List<Spot>> {
+        return withContext(Dispatchers.IO) {
+            RetrofitInstance.api.getLevelSpots(levelId)
+        }
     }
 }
