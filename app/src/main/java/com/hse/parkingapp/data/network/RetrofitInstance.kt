1f9f3675b8ecd3e090a082e0b473befa811e6627
pasha <pashamedvedev03@gmail.com>
2023-02-11 19:59:45 +0300

    change ip address to connect to backend from mobile device

--- app/src/main/java/com/hse/parkingapp/data/network/RetrofitInstance.kt
+++ app/src/main/java/com/hse/parkingapp/data/network/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val baseUrl = "http://10.0.2.2:8080/"
+    private const val baseUrl = "http://192.168.1.34:8080/"
 
     private val client = OkHttpClient.Builder().apply {
         val logging = HttpLoggingInterceptor()
